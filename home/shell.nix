da1ca6b50a56ba5dce12cac5b5c8f2467a9f27f6
Vladimir Vialov <vyalov.v@gmail.com>
2025-04-19 23:12:06 +0200

    add bindkeys for tmux

--- home/shell.nix
+++ home/shell.nix
@@ -28,6 +28,8 @@
 
         bindkey '^[[H' beginning-of-line
         bindkey '^[[F' end-of-line
+        bindkey '^[[1~' beginning-of-line
+        bindkey '^[[4~' end-of-line
         bindkey '^[[3~' delete-char
 
         export PATH=$PATH:~/.cargo/bin/
