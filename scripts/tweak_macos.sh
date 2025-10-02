d5710d23bbee0332f8b25ec5a94ffd6972c7bc79
Vladimir Vialov <vyalov.v@gmail.com>
2025-10-02 22:36:10 +0200

    change aerospace settings

--- scripts/tweak_macos.sh
+++ scripts/tweak_macos.sh
@@ -128,4 +128,7 @@ defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
 # Disable autocorrect
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 
+# https://nikitabobko.github.io/AeroSpace/guide#a-note-on-mission-control
+defaults write com.apple.dock expose-group-apps -bool true
+
 killall Dock
